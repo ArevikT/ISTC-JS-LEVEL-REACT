a9e1ebca869d2a6dcd726a99499fb2c387fe6c43
ArevikT <areviktovmasyan7@gmail.com>
2019-03-14 21:17:55 +0400

    lesson Lifecycle

--- my-clock/src/App.js
+++ my-clock/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import Clock from "./components/showClock"
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    clicked:false
+  }
+  delete= ()=>{
+    this.setState({clicked:true}) ;
+  }
   render() {
     return (
       <div className="App">
-      
+         {this.state.clicked?null:<Clock />}
+         <button onClick = {this.delete}>Click</button>
       </div>
     );
   }
---
+++ my-clock/src/components/showClock.js
@@ -0,0 +1,42 @@
+import React, {Component,Fragment} from "react";
+
+class Clock extends Component {
+    state = {
+        date: new Date(),
+        clicked: true
+    }
+
+    componentDidMount(){
+       this.time = setInterval(()=> this.changeTime(),1000)
+    }
+
+    changeTime = () => {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    componentDidUpdate(previousProps,previousState){
+        if(previousState.date !== this.state.date){
+            console.log("jam@ poxvec");
+        }
+     
+    }
+  
+    componentWillUnmount(){
+        clearInterval(this.time);
+    }
+
+
+
+    render(){
+        return(
+            <Fragment>
+                <h1>Hello erexeq jan</h1>
+                <p>the time is { this.state.date.toLocaleTimeString()}</p>
+                
+            </Fragment>
+        )
+    }
+}
+export default Clock;
