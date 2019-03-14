ed6324d1b3a05d1fd2d07cd63d7daf48c205535f
ArevikT <areviktovmasyan7@gmail.com>
2019-03-14 19:23:53 +0400

    creat new app

--- my-react/src/cubic/cub.js
+++ my-react/src/cubic/cub.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component , Fragment } from 'react';
 import './style.css';
 
 class Cubic extends Component {
     state ={
         color:"red",
-        count:0
+        count:0,
+        text: "",
+        number: 0
     }
     myFunction= ()=>{
         this.setState({
@@ -13,9 +15,26 @@ class Cubic extends Component {
         })
       
     }
+
+    changeText = (e) => {
+        this.setState({text:e.target.value}) 
+            
+    }
+    changeNumber = (e) => {
+        this.setState({text:e.target.value})
+    }
+
+
     render(){
         return (
+        <Fragment>
             <div style ={{backgroundColor: this.state.color}} className= "cubic" onClick= {this.myFunction}>{this.state.count}</div>
+
+            <input onChange = { this.changeText() } type = "text" ref = { el => this.text = el } value = {this.state.text} name = "text" />
+            <p>{ this.state.color.text }</p>
+            <input onChange = { this.changeNumber() } type = "number" ref = { el => this.number = el } value = "" name = "number" />
+            <p>{ this.state.color.number }</p>
+        </Fragment>
         )
     }
 
